4917467b67cbe02848b5e0cb3801a4953f9d2294
Carl-Johan Höglind <chd16002@student.mdh.se>
2020-01-21 14:31:16 +0100

    Added player sprite

--- game.py
+++ game.py
@@ -13,12 +13,36 @@ class background:
   def draw(self):
     screen.blit(sBackground, (0, 0))
 
+class turtle:
+  x = 100
+  y = 350
+  speed = 0
+  maxSpeed = 0
+  
+  def __init__(self):
+    pass
+  
+  def drawTurtle(self):
+    screen.blit(turtleImage, (self.x, self.y))
 
-screen = pygame.display.set_mode((347, 260))
+
+
+
+
+## Create background image
+#screen = pygame.display.set_mode((347, 260))
+screen = pygame.display.set_mode((1280, 720))
 sBackground = pygame.image.load("./images/background.png")
+sBackground = pygame.transform.scale(sBackground, (1280, 720))
+
+
+## Create turtle image
+turtleImage = pygame.image.load("./images/dino.png")
+
 
 bg = background(0, 0, sBackground, screen)
-pygame.display.set_caption("Frog RUN!")
+t = turtle()
+pygame.display.set_caption("Turtle RUN!")
 
 def gameLoop():
 
@@ -30,6 +54,7 @@ def gameLoop():
             running = False
 
     bg.draw()
+    t.drawTurtle()
     pygame.display.update()
 
 gameLoop()
